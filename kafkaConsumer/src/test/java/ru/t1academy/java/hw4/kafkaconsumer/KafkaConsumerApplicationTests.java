cc5ed7c987ccf096829c57e83426b8f619b84d3a
Georgiy Rusakov <gosha1510@yandex.ru>
2024-08-27 23:59:23 +0300

    init commit

---
+++ kafkaConsumer/src/test/java/ru/t1academy/java/hw4/kafkaconsumer/KafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.t1academy.java.hw4.kafkaconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ kafkaProducer/src/main/java/ru/t1academy/java/hw4/kafkaproducer/KafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package ru.t1academy.java.hw4.kafkaproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaProducerApplication.class, args);
+    }
+
+}
